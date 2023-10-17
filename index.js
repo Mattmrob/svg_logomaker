188a5dda6934be1f1f4390a53008ff4ad1cd28c3
Mysmelf <matthewr9937@gmail.com>
2023-10-17 11:08:41 -0700

    Created initial inquirer questioning for user to pick text, shape, and colors

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-const Shapes = require("./lib/shapes");
 const inquirer = require('inquirer');
+const Shapes = require("./lib/shapes");
 const fs = require('fs');
+
+
+const init = () => {
+
+inquirer.prompt([
+
+    {
+        type: 'input',
+        name: "text",
+        message: "Please enter up to three characters for your logo"
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "What color would you like the text to be?",
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: "What shape would you like your logo's background to be?",
+        choices: ["Circle", "Square", "Triangle"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: 'What color would you like the background shape to be?',
+    },
+]).then(answers => {
+    console.log(answers)
+    })
+
+}
+
+init();
