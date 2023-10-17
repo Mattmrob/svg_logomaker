6de013847b376727c708445ed8fd3bfdfb92fd9f
Mysmelf <matthewr9937@gmail.com>
2023-10-17 12:44:54 -0700

    successfully linked multiple shape classes as a single object to index.js

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 const inquirer = require('inquirer');
 const Shapes = require("./lib/shapes.js");
+// Shapes includes Circle, Square, and Triangle
 const RenderShape = require("./lib/renderShape.js")
 const fs = require('fs');
 
 
+
 const init = () => {
 
-inquirer.prompt([
+    const test = new Shapes.Circle("this", "is", "a", "test")
+    console.log(test);
 
-    {
-        type: 'input',
-        name: "text",
-        message: "Please enter up to three characters for your logo",
-    },
-    {
-        type: "input",
-        name: "textColor",
-        message: "Please enter a color keyword or hexadecimal for your text's color.",
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: "What shape would you like your logo's background to be?",
-        choices: ["Circle", "Triangle", "Square"]
-    },
-    {
-        type: "input",
-        name: "shapeColor",
-        message: "Please enter a color keyword or hexadecimal for your shape's color.",
-    },
-]).then(res => {
+// inquirer.prompt([
 
-    if (res.text.length > 3) {
-        return console.log("Logo cannot be more than three characters long!")
-    } else if (res.text.length === 0) {
-        return console.log("You must include at least one character for your logo!")
-    }
+//     {
+//         type: 'input',
+//         name: "text",
+//         message: "Please enter up to three characters for your logo",
+//     },
+//     {
+//         type: "input",
+//         name: "textColor",
+//         message: "Please enter a color keyword or hexadecimal for your text's color.",
+//     },
+//     {
+//         type: 'list',
+//         name: 'shape',
+//         message: "What shape would you like your logo's background to be?",
+//         choices: ["Circle", "Triangle", "Square"]
+//     },
+//     {
+//         type: "input",
+//         name: "shapeColor",
+//         message: "Please enter a color keyword or hexadecimal for your shape's color.",
+//     },
+// ]).then(res => {
 
-    const renderShape = new RenderShape();
-    renderShape.render(res);
+//     if (res.text.length > 3) {
+//         return console.log("Logo cannot be more than three characters long!")
+//     } else if (res.text.length === 0) {
+//         return console.log("You must include at least one character for your logo!")
+//     }
 
-    // const shape = new Shapes(res.text, res.textColor, res.shape, res.shapeColor)
+//     const renderShape = new RenderShape();
+//     renderShape.render(res);
 
-    // console.log(shape);
+//     // const shape = new Shapes(res.text, res.textColor, res.shape, res.shapeColor)
 
-    })
+//     // console.log(shape);
+
+//     })
 
 }
 
